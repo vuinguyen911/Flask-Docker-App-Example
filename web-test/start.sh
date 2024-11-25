09def2e9c19cb3f156bec2f7de3da90ee006673f
Vui Nguyen <vnv1004@gmail.com>
2024-11-25 17:16:52 +0900

    add log

--- web-test/start.sh
+++
@@ -1,8 +0,0 @@
-mkdir ssl
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout ssl/server.key -out ssl/server.crt
-
-# Build the Docker image
-docker build -t my-nginx .
-
-# Run the container
-docker run -d -p 80:80 -p 443:443 --name my-nginx-container my-nginx
